1de15ee789179997869de2695bc34d761eb23288
jorhel-oliveira <jorhel_psf@hotmail.com>
2021-08-12 20:29:52 -0300

    included transactional on service class

--- src/main/java/api/rest/service/MovieService.java
+++ src/main/java/api/rest/service/MovieService.java
@@ -9,6 +9,7 @@ import api.rest.requests.MoviePutRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -32,6 +33,7 @@ public class MovieService {
                 .orElseThrow(()-> new BadRequestException("Movie id not found"));
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public Movie save(MoviePostRequestBody moviePostRequestBody) {
         return movieRepository.save(MovieMapper.INSTANCE.toMovie(moviePostRequestBody));
     }
@@ -40,6 +42,7 @@ public class MovieService {
         movieRepository.delete(findByIdOrThrowBadRequestException(id));
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public void replace(MoviePutRequestBody moviePutRequestBody) {
         Movie savedMovie = findByIdOrThrowBadRequestException(moviePutRequestBody.getId());
         Movie movie = MovieMapper.INSTANCE.toMovie(moviePutRequestBody);
